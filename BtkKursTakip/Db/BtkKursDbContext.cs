7679a208c9b3460d226b08957dac2912e6dec257
Mustafa KURAL <mustafakural@outlook.com>
2024-11-09 18:53:59 +0300

    modelCreating ile ilişkiler oluşturuldu

---
+++ BtkKursTakip/Db/BtkKursDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BtkKursTakip.Modeller;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkKursTakip.Db
+{
+    public class BtkKursDbContext : DbContext
+    {
+        public DbSet<DbKurs> Kurslar {  get; set; }
+        public DbSet<DbEgitmen> Egitmenler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;" +
+                "Initial Catalog=BtkKurs_Db;Integrated Security=True;" +
+                "Encrypt=False;Trust Server Certificate=False;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DbKurs>() //bir kursun
+                .HasOne(e => e.Egitmen) //bir eğitmeni var
+                .WithMany(e => e.Kurslar)//eğitmenin ise birçok kursu var 
+                .HasForeignKey(e=>e.EgitmenId)//Kurslar tablosunda Yabancı anahtar
+                .HasPrincipalKey(e=>e.Id);//Egitmenler tablosunda birincil anahtar 
+                
+        }
+    }
+}
