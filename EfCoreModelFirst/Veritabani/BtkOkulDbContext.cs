f6c7c2507ff2a935ae7180c1e280e7ba9ea1a7e1
Mustafa KURAL <mustafakural@outlook.com>
2024-11-09 16:06:57 +0300

    model creating

---
+++ EfCoreModelFirst/Veritabani/BtkOkulDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EfCoreModelFirst.Modeller;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EfCoreModelFirst.Veritabani
+{
+    public class BtkOkulDbContext : DbContext
+    {
+        public DbSet<Sinif> Siniflar { get; set; }
+        public DbSet<Ogrenci> Ogrenci { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=" +
+                "btk_okul_db;trusted_connection=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sinif>()
+                .HasMany(e => e.Ogrenciler)
+                .WithOne(e => e.Sinifi)
+                .HasForeignKey(e => e.SinifId)
+                .HasPrincipalKey(e => e.Id);
+
+        }
+    }
+}
