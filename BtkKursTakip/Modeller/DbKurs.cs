bae9124e5b288ddc8e78ce28f7b2b980958bac39
Mustafa KURAL <mustafakural@outlook.com>
2024-11-09 18:23:25 +0300

    btk kurs uygulaması

---
+++ BtkKursTakip/Modeller/DbKurs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkKursTakip.Modeller
+{
+    public class DbKurs
+    {
+        public int Id { get; set; }//PK (Otomatik Artan)
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+        public DateTime Tarih { get; set; }
+        public int SehirId { get; set; }
+        public int IlceId { get; set; }
+        public string KursYeri { get; set; }
+        public int EgitmenId { get; set; }//FK (Yabancı Anahtar)
+    }
+}
