fbef4a64e1656564ac90e177903c362b515be973
Mustafa KURAL <mustafakural@outlook.com>
2024-11-10 15:54:17 +0300

    kullanıcı Modeli

---
+++ BtkKursTakip/Modeller/Kullanici.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkKursTakip.Modeller
+{
+    public enum YetkiTuru { Yonetici = 0, Kullanici = 1};
+    public class Kullanici
+    {
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Parola { get; set; }
+        public YetkiTuru Yetki { get; set; } // 0 - Yönetici 1-Normal Kullanıcı
+    }
+}
